16487f9a17711bfb01d6cc5b3ffb8faea9de29b9
nespolisimmaco <snespoli@yahoo.com>
2023-03-07 16:06:33 +0100

    Add: User name

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Chiedi all'utente il suo nome
+// Poi chiedi il suo cognome
+// Poi il suo colore preferito
+// Scrivere sulla pagina la password nomecognomecolorepreferito21
+
+// Chiedi il nome all'utente
+const userName = prompt("Ciao utente! Qual è il tuo nome?")
+console.log("Nome:", userName)
