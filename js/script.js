17fadd8c45b8e3cec40b43f0c2eb370dfaf16747
nespolisimmaco <snespoli@yahoo.com>
2023-03-07 16:28:51 +0100

    Add: User favourite color

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@
 // Scrivere sulla pagina la password nomecognomecolorepreferito21
 
 // Chiedo il nome all'utente
-const userName = prompt("Ciao utente! Qual è il tuo nome?")
-console.log("Nome:", userName)
+const userName = prompt("Ciao utente! Qual è il tuo nome?");
+console.log("Nome:", userName);
 
 // Chiedo il cognome all'utente
-const userLastName = prompt("Qual è il tuo cognome?")
-console.log("Cognome:", userLastName)
+const userLastName = prompt("Qual è il tuo cognome?");
+console.log("Cognome:", userLastName);
+
+// Chiedo il colore preferito all'utente
+const favouriteColor = prompt("Qual è il tuo colore preferito?");
+console.log("Colore:", favouriteColor);
